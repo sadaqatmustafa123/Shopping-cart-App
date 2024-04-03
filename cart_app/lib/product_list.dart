e0503a8eeec7ff0a956df7557b692ce686c283d1
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2024-04-03 23:09:26 +0500

    initial commit

---
+++ cart_app/lib/product_list.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:flutter/widgets.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({Key? key}) : super(key: key);
+
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Grapes',
+    'Banana',
+    'Cherry',
+    'Peach',
+    'Mixed Fruit Basket',
+  ];
+  List<String> productUnit = [
+    'KG',
+    'Dozen',
+    'KG',
+    'Dozen',
+    'KG',
+    'KG',
+    'KG',
+  ];
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+  List<String> productImage = [
+    'https://images.unsplash.com/photo-1553279768-865429fa0078?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1547514701-42782101795e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1596363505729-4190a9506133?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1528821154947-1aa3d1b74941?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1602813812581-0713dae489da?q=80&w=1460&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1519996529931-28324d5a630e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product List'),
+        centerTitle: true,
+        actions: const [
+          Center(
+            child: badges.Badge(
+              badgeContent: Text(
+                '3',
+                style: TextStyle(color: Colors.white),
+              ),
+              child: Icon(Icons.shopping_bag),
+            ),
+          ),
+          SizedBox(
+            width: 8,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: productName.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Image(
+                                    height: 100,
+                                    width: 100,
+                                    image: NetworkImage(
+                                        productImage[index].toString())),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Column(children: [
+                                  Text(
+                                    productName[index].toString(),
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  Text(
+                                    productUnit[index].toString(),
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ]),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+}
